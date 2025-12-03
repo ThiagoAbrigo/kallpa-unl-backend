191e1dd0b409e0aee4f75b9629c67dccfc448c1d
Elia700 <elias.poma@unl.edu.ec>
2025-12-03 09:25:36 -0500

    Configuracion base de datos con Docker

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,16 @@ def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object(Config)
     db.init_app(app)
+    
     with app.app_context():
+        # Import models here to ensure they are registered with SQLAlchemy
+        from app.models import attendance, participant, program, responsible, schedule
+        from app.models import assessment, initialAssessment, aerobicAssessment, periodicTest
+        
         db.create_all()
+        
+        # Register blueprints
+        from app.routes.attendance_routes import attendance_bp
+        app.register_blueprint(attendance_bp, url_prefix='/api')
 
     return app
--- app/models/aerobicAssessment.py
+++ app/models/aerobicAssessment.py
@@ -3,8 +3,12 @@ from app import db
 
 class AerobicAssessment(Assessment):
     __tablename__ = 'aerobic_assessment'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, db.ForeignKey('assessment.id'), primary_key=True)
     testCooperDistancia = db.Column(db.Integer)
     testNavetteNivel = db.Column(db.Integer)
     frecuenciaCardiacaReposo = db.Column(db.Integer)
     frecuenciaCardiacaPostEjercicio = db.Column(db.Integer)
+    
+    __mapper_args__ = {
+        'polymorphic_identity': 'aerobic_assessment',
+    }
